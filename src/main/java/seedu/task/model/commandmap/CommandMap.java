0fbd9c9db80d82b36ce85ff63421d2c1e3c04d4d
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-04-09 22:31:35 +0800

    Add Cal and ShowAlias commands to CommandMap

--- src/main/java/seedu/task/model/commandmap/CommandMap.java
+++ src/main/java/seedu/task/model/commandmap/CommandMap.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import seedu.task.logic.commands.AddCommand;
 import seedu.task.logic.commands.AliasCommand;
+import seedu.task.logic.commands.CalCommand;
 import seedu.task.logic.commands.CompleteCommand;
 import seedu.task.logic.commands.DeleteCommand;
 import seedu.task.logic.commands.EditCommand;
@@ -21,6 +22,7 @@ import seedu.task.logic.commands.RedoCommand;
 import seedu.task.logic.commands.ResetCommand;
 import seedu.task.logic.commands.RevertCommand;
 import seedu.task.logic.commands.SelectCommand;
+import seedu.task.logic.commands.ShowAliasCommand;
 import seedu.task.logic.commands.UndoCommand;
 import seedu.task.logic.commands.UnrevertCommand;
 import seedu.task.logic.commands.UseCommand;
@@ -61,6 +63,8 @@ public class CommandMap {
         commandWords.add(RevertCommand.COMMAND_WORD);
         commandWords.add(UnrevertCommand.COMMAND_WORD);
         commandWords.add(UseCommand.COMMAND_WORD);
+        commandWords.add(CalCommand.COMMAND_WORD);
+        commandWords.add(ShowAliasCommand.COMMAND_WORD);
 
         return commandWords;
     }
